69f5efce30c78cca70d9df0cab206f847a0aa1a3
Ronaldo <ronaldo_so10@hotmail.com>
2026-06-13 10:28:11 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+// FABRICA = classe auxiliar responsavel por instanciar os DAOS
+public class DaoFactory {
+
+	/*
+	 * VAI EXPOR UM METODO QUE RETORNA O TIPO DA INTERFACE(SELLERDAO),
+	 * mas internamente ela vai instanciar uma implementacao
+	 * nao expoe a implementacao
+	 */
+	public static SellerDAO createSellerDao() {
+		return new SellerDaoJDBC();
+	}
+}
---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,37 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDAO;
+import model.entities.Seller;
+
+
+// essa classe é a implementação do JDBC do sellerDao
+public class SellerDaoJDBC implements SellerDAO{
+
+	@Override
+	public void insert(Seller obj) { 
+		
+	}
+
+	@Override
+	public void update(Seller obj) { 
+		
+	}
+
+	@Override
+	public void deleteById(Seller obj) { 
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) { 
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() { 
+		return null;
+	}
+
+}
